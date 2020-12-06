74dde409712c832e04cc916a411f7a4c5fc39d10
Ashish Babar <ashishbabar101@gmail.com>
2020-12-06 14:54:07 +0530

    OwnYourHealth : Package chaincode script

--- scripts/civil/deploy-chaincode.sh
+++ scripts/civil/deploy-chaincode.sh
@@ -1,5 +1,27 @@
-cd ../../chaincode/storage
+export CORE_PEER_TLS_ENABLED=true
+export FABRIC_CFG_PATH=${PWD}
+export CHANNEL_NAME=general-medicine-channel
+export ORDERER_CA=${PWD}/../../organizations/ordererOrganizations/solapurhcareorderer.in/orderers/solapurhcareorderer.in/msp/tlscacerts/tlsca.solapurhcareorderer.in-cert.pem
+export CIVIL_CA=${PWD}/../../organizations/peerOrganizations/scsmsr.co.in/peers/scsmsr.co.in/tls/ca.crt
 
-go mod init storage.go
+# export CORE_PEER_LOCALMSPID="SCSMSRMSP"
+# export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../../organizations/ordererOrganizations/solapurhcareorderer.in/orderers/solapurhcareorderer.in/msp/tlscacerts/tlsca.solapurhcareorderer.in-cert.pem
+# export CORE_PEER_MSPCONFIGPATH=${PWD}/../../organizations/ordererOrganizations/solapurhcareorderer.in/users/Admin@solapurhcareorderer.in/msp
 
+export CORE_PEER_LOCALMSPID="SCSMSRMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$CIVIL_CA
+export CORE_PEER_MSPCONFIGPATH=${PWD}/../../organizations/peerOrganizations/scsmsr.co.in/users/Admin@scsmsr.co.in/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+export CC_NAME=simpleContract
+export CC_SRC_PATH=../../chaincode/simple-contract
+export CC_RUNTIME_LANGUAGE=golang
+export CC_VERSION=1.0
+cd ../../chaincode/simple-contract
+
+go mod init gitlab.com/ashishbabar/ownyourhealth/simple-contract
+go get -u github.com/hyperledger/fabric-contract-api-go
+go mod vendor
 go build
+
+peer lifecycle chaincode package ${CC_NAME}.tar.gz --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} --label ${CC_NAME}_${CC_VERSION} >&log.txt
